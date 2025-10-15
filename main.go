ea3f499c7a884006b4bc0e5297b5ff4e85ba2910
Amal Zakirov <116391159+azaiv@users.noreply.github.com>
2025-10-15 13:38:56 +0300

    Синтаксис. Создание переменных, объявление функций

--- main.go
+++ main.go
@@ -1,23 +1,37 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 func main() {
-	const IMTPower = 2
-	var userHeight float64 = 0
-	var userWidth float64 = 0
-	fmt.Println("Калькулятор индекса тела")
-	fmt.Println("Введите свой рост:")
-	fmt.Scan(&userHeight)
-	fmt.Println("Введите свой вес:")
-	fmt.Scan(&userWidth)
-	IMT := userWidth * math.Pow(userHeight, IMTPower)
-	fmt.Println(IMT)
+	createValuable()
+	withArgs(10, 10)
+	testString := withArgsAndOutput(10, 20)
+	fmt.Println(withArgsAndOutputs(testString, 20))
 }
 
-func secondary(userHeight float64, userSize int) {
-	
+// Создание переменных
+func createValuable() {
+	var test1 float64 = 100 // Явное указание типов данных
+	var test2 = 1.8         // Неявное указание данных
+	test3 := 1.8            // Наиболее часто используемое
+	const test4 = 1.8
+	fmt.Println(test1, test2, test3, test4)
+}
+
+// Объявление функций
+
+// Функция с аргументами
+func withArgs(arg1 float64, arg2 float64) {
+	fmt.Println(arg1, arg2)
+}
+
+// Функция с аргументами, и с выходным параметром
+func withArgsAndOutput(arg1 float64, arg2 float64) string {
+	fmt.Println(arg1, arg2)
+	return "test"
+}
+
+// Функция с аргументом и с двумя выходными параметрами
+func withArgsAndOutputs(arg1 string, arg2 float64) (string, string) {
+	return arg1, fmt.Sprint(arg2)
 }
